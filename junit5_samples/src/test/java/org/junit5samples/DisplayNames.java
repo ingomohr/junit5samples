787e04d615ee23cd5006d39a7143799163c54837
Ingo Mohr <tellastory73@gmail.com>
2017-12-13 00:00:48 +0100

    added displayname

---
+++ junit5_samples/src/test/java/org/junit5samples/DisplayNames.java
@@ -0,0 +1,21 @@
+package org.junit5samples;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DisplayNames {
+
+	@Test
+	@DisplayName("This is a test 😎")
+	void test() {
+	}
+
+	@ParameterizedTest(name = " >>{0}<<")
+	@ValueSource(strings = { "a", "b", "c" })
+	@DisplayName(value = "Candidate is an")
+	void foo(String candidate) {
+	}
+
+}
