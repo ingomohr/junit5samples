411ece608ce0c1059bb27d87e15c54e9a5b83453
Ingo Mohr <ingo.mohr@biotronik.com>
2017-12-13 10:30:56 +0000

    activated test exclusion by tags (pom)

--- junit5_samples/src/test/java/org/junit5samples/Repeat.java
+++ junit5_samples/src/test/java/org/junit5samples/Repeat.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 
 class Repeat {
 
 	@RepeatedTest(5)
+	@Tag("whackle-candidates")
 	void test() {
 		assertTrue(Math.random() < 0.8D);
 	}
